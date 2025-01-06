9d4f75354c235f52bf10ef219536996e24e3919f
fcortesbio <fcortesbio@gmail.com>
2025-01-06 10:09:14 -0500

    Add shortcuts.py

---
+++ shorcuts.py
@@ -0,0 +1,47 @@
+from functools import wraps
+
+# Decorator for input validation
+def validate_index(func):
+    """Validates that the first argument to a function is a non-negative integer.
+
+    This decorator checks if the first positional argument (`n`) passed to the 
+    decorated function is a non-negative integer. If not, it raises a ValueError.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Raises:
+        ValueError: If the first argument to the function is not a non-negative integer.
+
+    Returns:
+        Callable: The decorated function.
+    """
+    invalid_input_msg = "Input must be a non-negative integer."
+    @wraps(func)
+    def wrapper(n):
+        if not isinstance(n, int) or n < 0:
+            raise ValueError(invalid_input_msg)
+        return func(n)
+    return wrapper
+
+def memoize(func):
+    """Memoizes a function.
+
+    This decorator caches the results of a function based on its arguments. 
+    If the function is called again with the same arguments, the cached result 
+    is returned instead of recomputing.
+
+    Args:
+        func (Callable): The function to be memoized.
+
+    Returns:
+        Callable: The memoized function.
+    """
+    cache = {}
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return wrapper     
